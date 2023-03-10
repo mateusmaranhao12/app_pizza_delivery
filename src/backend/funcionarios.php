756cf31861232ac34048a5d405999029eece1726
Mateus Maranhão <feradourada@gmail.com>
2023-03-10 18:37:02 -0300

    update Formulario.vue and add db_close.php

--- src/backend/funcionarios.php
+++ src/backend/funcionarios.php
@@ -117,10 +117,6 @@
         }
      }
  
-     $db_conn->close();
-    //simple pass a json format data in php 
-    header("Content-type: application/json");
-    echo json_encode($res_output);
-    die();
+     include "db_close.php";
 
 ?>
--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -4,43 +4,32 @@
         <h2 class="mt-5 mb-5">Etapa 1: Informações do cliente</h2>
         <div class="form-group mx-2 mb-3">
             <label for="nome">Nome</label>
-            <input type="text" placeholder="Nome do cliente" ref="nome" class="form-control" name="nome" />
+            <input type="text" v-model="lista_clientes.nome" placeholder="Nome do cliente" ref="nome" class="form-control" name="nome" />
         </div>
         <div class="form-group mx-2 mb-3">
             <label for="email">E-mail</label>
-            <input type="text" placeholder="E-mail do cliente" ref="email" class="form-control" name="email" />
+            <input type="text" v-model="lista_clientes.email" placeholder="E-mail do cliente" ref="email" class="form-control" name="email" />
         </div>
         <div class="form-group mx-2 mb-3">
             <label for="telefone">Telefone</label>
-            <input type="tel" placeholder="Telefone do cliente" ref="telefone" v-maska="'(##) #####-####'" class="form-control" name="telefone" />
+            <input type="tel" v-model="lista_clientes.telefone" placeholder="Telefone do cliente" ref="telefone" v-maska="'(##) #####-####'"
+                class="form-control" name="telefone" />
         </div>
         <div class="form-group mx-2 mb-3">
             <label for="sabor_pizza">Sabor de pizza: </label>
-            <select class="form-select" name="sabor_pizza" id="sabor_pizza">
+            <select class="form-select" v-model="lista_clientes.sabor_pizza" name="sabor_pizza" id="sabor_pizza">
                 <option value="">Escolha um sabor</option>
-                <option value="margherita">
-                    Margherita
-                </option>
-                <option value="frango_catupiry">
-                    Frango c/ Catupiry
-                </option>
-                <option value="quatro_queijos">
-                    4 queijos
+                <option v-for="p in sabor_pizza" :key="p" :value="p">
+                    {{ p }}
                 </option>
             </select>
         </div>
         <div class="form-group mx-2 mb-3">
             <label for="borda_pizza">Borda da pizza: </label>
-            <select class="form-select" name="borda_pizza" id="borda_pizza">
+            <select class="form-select" v-model="lista_clientes.borda_pizza" name="borda_pizza" id="borda_pizza">
                 <option value="">Escolha um tipo de borda</option>
-                <option value="normal">
-                    Normal
-                </option>
-                <option value="catupiry">
-                    Catupiry
-                </option>
-                <option value="cheddar">
-                    Cheddar
+                <option v-for="b in borda_pizza" :key="b" :value="b">
+                    {{ b }}
                 </option>
             </select>
         </div>
@@ -54,15 +43,19 @@
         <h2 class="mt-5 mb-5">Etapa 2: Endereço do cliente</h2>
         <div class="form-group col-md-12 mx-2 mb-3">
             <label for="rua">Rua</label>
-            <input type="text" placeholder="Rua" ref="rua" class="form-control" name="rua" />
+            <input type="text" v-model="lista_clientes.rua" placeholder="Rua" ref="rua" class="form-control" name="rua" />
         </div>
         <div class="form-group col-md-12 mx-2 mb-3">
             <label for="numero">Número</label>
-            <input type="text" placeholder="Número" ref="numero" class="form-control" name="numero" />
+            <input type="text" v-model="lista_clientes.numero" placeholder="Número" ref="numero" class="form-control" name="numero" />
         </div>
         <div class="form-group mx-2 mb-3">
             <label for="complemento">Complemento</label>
-            <input type="text" placeholder="Complemento" ref="complemento" class="form-control" name="complemento" />
+            <input type="text" v-model="lista_clientes.complemento" placeholder="Complemento" ref="complemento" class="form-control" name="complemento" />
+        </div>
+        <div class="form-group mx-2 mb-3">
+            <label for="complemento">Bairro</label>
+            <input type="text" v-model="lista_clientes.bairro" placeholder="Bairro" ref="bairro" class="form-control" name="bairro" />
         </div>
 
         <button class="btn btn-warning float-end mt-5 mb-5" @click="finalizarPedido()">Finalizar Pedido
@@ -80,7 +73,22 @@ export default {
 
     data() {
         return {
-            etapa: 1
+            etapa: 1,
+            sabor_pizza: ['4 Queijos', 'Frango com Catupiry', 'Margherita'],
+            borda_pizza: ['Cheddar', 'Catupiry', 'Normal'],
+            clientes: [],
+            lista_clientes: {
+                nome: '',
+                email: '',
+                telefone: '',
+                sabor_pizza: '',
+                borda_pizza: '',
+                rua: '',
+                numero: '',
+                complemento: '',
+                bairro: ''
+
+            }
         }
     },
 
@@ -103,5 +111,5 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-    @import '../scss/adicionar_pedido.scss';
+@import '../scss/adicionar_pedido.scss';
 </style>
