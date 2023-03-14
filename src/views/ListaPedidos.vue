2a12cdb652d6e351b227ae0a22612dd90895c5e2
Mateus Maranhão <feradourada@gmail.com>
2023-03-14 10:53:31 -0300

    implement watch ordenacao

--- src/views/ListaPedidos.vue
+++ src/views/ListaPedidos.vue
@@ -5,6 +5,16 @@
     </div>
     <div class="container">
         <div class="row">
+            <h2 class="mt-3 col-md-6">Filtrar pedidos</h2>
+            <div class="col-md-6 mt-3">
+                <select class="form-select" aria-label="Filtrar" v-model="ordenacao">
+                    <option value="">Selecione uma opção</option>
+                    <option value="1">Ordem alfabética</option>
+                    <option value="2">Ordem alfabética inversa</option>
+                    <option value="3">Ordem por ID inversa</option>
+                    <option value="4">Ordenar por ID (Padrão)</option>
+                </select>
+            </div>
             <div class="col">
                 <table class="table table-striped table-hover mt-5">
                     <thead>
@@ -53,8 +63,77 @@ export default {
 
     data() {
         return {
-            clientes: []
+            clientes: [],
+            ordenacao: ''
         }
+    },
+
+    watch: {
+
+        ordenacao(valorNovo) {
+
+            if (valorNovo == 1) { //ordenação por ordem alfabética
+
+                this.clientes.sort((proximo, atual) => {
+                    //1 caso a ordem esteja correta
+                    if (atual.nome < proximo.nome) {
+                        return 1
+                    }
+                    //-1 caso a ordem esteja incorreta (precisa inverter as posições)
+                    if (atual.nome > proximo.nome) {
+                        return -1
+                    }
+                    //0 caso nenhuma acao seja necessaria
+                    return 0
+
+                })
+
+            } else if (valorNovo == 2) { //inverter ordem alfabética
+                this.clientes.sort((proximo, atual) => {
+                    //1 caso a ordem esteja correta
+                    if (atual.nome > proximo.nome) {
+                        return 1
+                    }
+                    //-1 caso a ordem esteja incorreta (precisa inverter as posições)
+                    if (atual.nome < proximo.nome) {
+                        return -1
+                    }
+                    //0 caso nenhuma acao seja necessaria
+                    return 0
+
+                })
+            } else if (valorNovo == 3) { //ordenar ID inverso
+                this.clientes.sort((proximo, atual) => {
+                    //1 caso a ordem esteja correta
+                    if (atual.ID > proximo.ID) {
+                        return 1
+                    }
+                    //-1 caso a ordem esteja incorreta (precisa inverter as posições)
+                    if (atual.ID < proximo.ID) {
+                        return -1
+                    }
+                    //0 caso nenhuma acao seja necessaria
+                    return 0
+
+                })
+            } else {
+                this.clientes.sort((proximo, atual) => {
+                    //1 caso a ordem esteja correta
+                    if (atual.ID < proximo.ID) {
+                        return 1
+                    }
+                    //-1 caso a ordem esteja incorreta (precisa inverter as posições)
+                    if (atual.ID > proximo.ID) {
+                        return -1
+                    }
+                    //0 caso nenhuma acao seja necessaria
+                    return 0
+
+                })
+            }
+
+        }
+
     },
 
     mounted() {
